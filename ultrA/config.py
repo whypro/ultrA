275b6e7c50e20a9113ce46fac90392e57d87db0e
whypro <whypro@live.cn>
2014-04-30 12:11:06 +0800

    前台细节修正。

--- ultrA/config.py
+++ ultrA/config.py
@@ -16,8 +16,8 @@ DB_NAME = 'ultrA'
 
 if 'Windows' in platform.system():
     # MEDIA_PATH = 'I:\\一些资料\\【图片】\\【互联网图片】\\'
-    # MEDIA_PATH = 'F:\\apple\\autoed\\'
-    MEDIA_PATH = 'E:\\test\\'
+    MEDIA_PATH = 'F:\\apple\\autoed\\'
+    # MEDIA_PATH = 'E:\\test\\'
 else:
     # MEDIA_PATH = '/var/run/media/whypro/4. Backups/apple/autoed/'
     MEDIA_PATH = '/var/run/media/whypro/My Book/一些资料/【图片】/【互联网图片】/'
