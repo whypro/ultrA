6c3f15cef5209f708551434f8f8e549075b80ce6
whypro <whypro@live.cn>
2014-05-03 10:58:06 +0800

    增加评分功能； 增加删除 Topic 功能； 优化首页排序规则。

--- ultrA/views.py
+++ ultrA/views.py
@@ -32,9 +32,23 @@ def show_all_topics(tag=None):
     topic_collection = client[current_app.config['DB_NAME']][current_app.config['TOPIC_COLLECTION']]
     tags = topic_collection.distinct('tags')
     if not tag:
-        topics = topic_collection.find({}, {'title': True, 'images': True}).sort([('create_time', -1), ('_id', -1)]).limit(50)
+        topics = topic_collection.find(
+            {'deleted': {'$ne': True}},
+            {'title': True, 'images': True}
+        ).sort([
+            ('click_count', 1), 
+            #('rate', -1),
+            ('_id', -1)
+        ]).limit(50)
     elif tag in tags:
-        topics = topic_collection.find({'tags': tag}, {'title': True, 'images': True}).sort([('create_time', -1), ('_id', -1)]).limit(50)
+        topics = topic_collection.find(
+            {'tags': tag, 'deleted': {'$ne': True}},
+            {'title': True, 'images': True}
+        ).sort([
+            ('click_count', 1), 
+            #('rate', -1),
+            ('_id', -1)
+        ]).limit(50)
     else:
         abort(404)
         
@@ -52,7 +66,7 @@ def show_topic(oid):
     rate = topic.get('rate')
     images = image_collection.find({'_id': {'$in': [image for image in topic['images']]}})
     print(images.count())
-    
+    topic_collection.update({'_id': ObjectId(oid)}, {'$inc': {'click_count': 1}})
     return render_template('show_topic.html', topic=topic, images=images, rate=rate)
 
 
@@ -80,6 +94,8 @@ def show_image(oid):
     client = MongoClient()
     image_collection = client[current_app.config['DB_NAME']][current_app.config['IMAGE_COLLECTION']]
     image = image_collection.find_one({'_id': ObjectId(oid)})
+    if not image:
+        abort(404)
     path = os.path.join(current_app.config['MEDIA_PATH'], image['path'])
     directory = os.path.dirname(path)
     filename = os.path.basename(path)
@@ -179,6 +195,28 @@ def import_topics_and_images(path, topic_collection, image_collection, option=0)
 
                                 image_id = image_collection.find_one({'path': rel_image_path}, {'_id': True}).get('_id')
                                 topic_collection.update({'title': topic}, {'$addToSet': {'images': image_id}})
+
+import shutil
+
+@frontend.route('/topic/<oid>/_delete/', methods=['POST'])
+def delete_topic(oid):
+    # remove_local = request.form.get('remove_local')
+    client = MongoClient()
+    topic_collection = client[current_app.config['DB_NAME']][current_app.config['TOPIC_COLLECTION']]
+    image_collection = client[current_app.config['DB_NAME']][current_app.config['IMAGE_COLLECTION']]
+    topic = topic_collection.find_one({'_id': ObjectId(oid)})
+    first_image = image_collection.find_one({'_id': {'$in': topic['images']}})
+    # 删除图片目录
+    if first_image:
+        topic_path = os.path.dirname(os.path.join(current_app.config['MEDIA_PATH'], first_image['path']))
+        print(topic_path)
+        if os.path.exists(topic_path):
+            shutil.rmtree(topic_path)
+    # 删除所有图片
+    image_collection.remove({'_id': {'$in': topic['images']}})
+    # 标记主题为已删除
+    topic_collection.update({'_id': ObjectId(oid)}, {'$set': {'deleted': True}})
+    return jsonify({})
 
 
 @frontend.route('/topic/<oid>/_edit/', methods=['POST'])
